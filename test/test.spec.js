b4f5c74fd4c9d4ca4572441804f5eb6f5cab9921
John Yagiz <john.yagiz@gmail.com>
2022-05-03 18:11:50 -0700

    Add tests to build

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,24 +1,9 @@
 import { expect } from 'chai';
-// import calcUtil from '../src/util/calcUtil.ts';
 import calcUtil from '../src/util/calcUtilv2.ts';
-// const calcUtil = require("../src/util/calcUtil.ts");
-// const testUtil = require("../src/util/testUtil.ts");
-
-
-// console.log(testUtil)
-
-// const test = require('@unfocused/spawn').default;
-// const test = require('@unfocused/treasure-goblin/src/util/common.ts').default;
-// import Spawn from '@unfocused/spawn';
-// import _ from '@unfocused/treasure-goblin';
 
 describe('Utils', function () {
-  it('computeInput', () => {
-    // const { total } = calcUtil.computeValue(0, 0, 0);
-    // const { total } = calcUtil.computeInput('5 + 5');
-    const x = calcUtil.getNestedInputs('0');
-    // const x = testUtil.test();
-    expect(x).to.deep.equal(['0']);
-    // expect('pizza-time').to.equal('pizza-time');
+  it('getNestedInputs', () => {
+    const value = calcUtil.getNestedInputs('0');
+    expect(value).to.deep.equal(['0']);
   })
 });
